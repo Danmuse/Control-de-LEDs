094fd900b17b9c09eb35b688db6c097018ee2e15
Danmuse <danifabriziodmodica@gmail.com>
2021-11-05 11:15:26 -0300

    Improve Firebase connection

--- firebase.js
+++ firebase.js
@@ -1,16 +1,14 @@
-import { initializeApp } from 'firebase/app';
-import { getDatabase } from "firebase/database";
+import { initializeApp } from 'firebase/app'
+import { getDatabase } from 'firebase/database'
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDSGxlI_DrLMlVmPfzw-uj6QiK3qa2GQeM",
-  authDomain: "control-de-leds.firebaseapp.com",
-  databaseURL: "https://control-de-leds-default-rtdb.firebaseio.com",
-  storageBucket: "control-de-leds.appspot.com",
-};
+  apiKey: 'AIzaSyDSGxlI_DrLMlVmPfzw-uj6QiK3qa2GQeM',
+  authDomain: 'control-de-leds.firebaseapp.com',
+  databaseURL: 'https://control-de-leds-default-rtdb.firebaseio.com',
+  storageBucket: 'control-de-leds.appspot.com',
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const database = getDatabase(app);
-
-
+const app = initializeApp(firebaseConfig)
+export const database = getDatabase(app)
