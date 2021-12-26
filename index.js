cf6801965fca2f52e2a6fade712d3f6178fe1677
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-12-26 15:01:29 -0300

    Update code for performance purposes

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-import { database } from './firebase'
+import { database } from './firebase.js'
 import { ref, set } from 'firebase/database'
 
-function WriteLedData(LED_id, status) {
-  set(ref(database, 'LEDs/' + LED_id), {
-    status: status,
-  })
-}
+const $ = param => document.getElementById(param)
 
 const LEDs = [
   {
@@ -22,10 +18,17 @@ const LEDs = [
   },
 ]
 
-const $ = (param) => document.getElementById(param)
+const BTN_RED = $('BTN_RED')
+const BTN_GREEN = $('BTN_GREEN')
+const BTN_BLUE = $('BTN_BLUE')
+
 const description_RED = $('description_RED')
 const description_GREEN = $('description_GREEN')
 const description_BLUE = $('description_BLUE')
+
+BTN_RED.addEventListener('click', () => Switch(LEDs[0]))
+BTN_GREEN.addEventListener('click', () => Switch(LEDs[1]))
+BTN_BLUE.addEventListener('click', () => Switch(LEDs[2]))
 
 Switch(LEDs[0])
 Switch(LEDs[1])
@@ -34,41 +37,37 @@ Switch(LEDs[2])
 function Switch({ ID, status }) {
   console.log('Estado de LEDs')
   WriteLedData(ID, !status)
-  LEDs.map((e) => {
+  LEDs.map(e => {
     return e.ID == ID ? (e.status = !status) : e
   })
-  console.log(LEDs[0].status)
-  console.log(LEDs[1].status)
-  console.log(LEDs[2].status)
-  if (LEDs[0].status == true) {
+  console.log({ status: LEDs[0].status })
+  console.log({ status: LEDs[1].status })
+  console.log({ status: LEDs[2].status })
+  if (LEDs[0].status) {
     document.documentElement.style.removeProperty('--grayscale-RED')
     description_RED.innerText = 'El LED está ENCENDIDO'
   } else {
     document.documentElement.style.setProperty('--grayscale-RED', 'grayscale()')
     description_RED.innerText = 'El LED está APAGADO'
   }
-  if (LEDs[1].status == true) {
+  if (LEDs[1].status) {
     document.documentElement.style.removeProperty('--grayscale-GREEN')
     description_GREEN.innerText = 'El LED está ENCENDIDO'
   } else {
-    document.documentElement.style.setProperty(
-      '--grayscale-GREEN',
-      'grayscale()'
-    )
+    document.documentElement.style.setProperty('--grayscale-GREEN', 'grayscale()')
     description_GREEN.innerText = 'El LED está APAGADO'
   }
-  if (LEDs[2].status == true) {
+  if (LEDs[2].status) {
     document.documentElement.style.removeProperty('--grayscale-BLUE')
     description_BLUE.innerText = 'El LED está ENCENDIDO'
   } else {
-    document.documentElement.style.setProperty(
-      '--grayscale-BLUE',
-      'grayscale()'
-    )
+    document.documentElement.style.setProperty('--grayscale-BLUE', 'grayscale()')
     description_BLUE.innerText = 'El LED está APAGADO'
   }
 }
 
-BTN_RED.addEventListener('click', () => Switch(LEDs[0]))
-BTN_GREEN.addEventListener('click', () => Switch(LEDs[1]))
-BTN_BLUE.addEventListener('click', () => Switch(LEDs[2]))
+function WriteLedData(LED_id, status) {
+  set(ref(database, 'LEDs/' + LED_id), {
+    status: status,
+  })
+}
