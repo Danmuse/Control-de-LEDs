c06927ee276dbf20038025c0912a1acb4bfe4741
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-12-26 15:01:57 -0300

    Update code for performance purposes

--- index.js
+++ index.js
@@ -3,28 +3,34 @@ import { ref, set } from 'firebase/database'
 
 const $ = param => document.getElementById(param)
 
+const description_RED = $('description_RED')
+const description_GREEN = $('description_GREEN')
+const description_BLUE = $('description_BLUE')
+
 const LEDs = [
   {
     ID: 'LED ROJO',
     status: true,
+    style: '--grayscale-RED',
+    textElement: description_RED,
   },
   {
     ID: 'LED VERDE',
     status: true,
+    style: '--grayscale-GREEN',
+    textElement: description_GREEN,
   },
   {
     ID: 'LED AZUL',
     status: true,
+    style: '--grayscale-BLUE',
+    textElement: description_BLUE,
   },
 ]
 
 const BTN_RED = $('BTN_RED')
 const BTN_GREEN = $('BTN_GREEN')
 const BTN_BLUE = $('BTN_BLUE')
-
-const description_RED = $('description_RED')
-const description_GREEN = $('description_GREEN')
-const description_BLUE = $('description_BLUE')
 
 BTN_RED.addEventListener('click', () => Switch(LEDs[0]))
 BTN_GREEN.addEventListener('click', () => Switch(LEDs[1]))
@@ -43,27 +49,16 @@ function Switch({ ID, status }) {
   console.log({ status: LEDs[0].status })
   console.log({ status: LEDs[1].status })
   console.log({ status: LEDs[2].status })
-  if (LEDs[0].status) {
-    document.documentElement.style.removeProperty('--grayscale-RED')
-    description_RED.innerText = 'El LED está ENCENDIDO'
-  } else {
-    document.documentElement.style.setProperty('--grayscale-RED', 'grayscale()')
-    description_RED.innerText = 'El LED está APAGADO'
-  }
-  if (LEDs[1].status) {
-    document.documentElement.style.removeProperty('--grayscale-GREEN')
-    description_GREEN.innerText = 'El LED está ENCENDIDO'
-  } else {
-    document.documentElement.style.setProperty('--grayscale-GREEN', 'grayscale()')
-    description_GREEN.innerText = 'El LED está APAGADO'
-  }
-  if (LEDs[2].status) {
-    document.documentElement.style.removeProperty('--grayscale-BLUE')
-    description_BLUE.innerText = 'El LED está ENCENDIDO'
-  } else {
-    document.documentElement.style.setProperty('--grayscale-BLUE', 'grayscale()')
-    description_BLUE.innerText = 'El LED está APAGADO'
-  }
+
+  LEDs.forEach(({ status, style, textElement }) => {
+    if (status) {
+      document.documentElement.style.removeProperty(style)
+      textElement.innerText = 'El LED está ENCENDIDO'
+    } else {
+      document.documentElement.style.setProperty(style, 'grayscale()')
+      textElement.innerText = 'El LED está APAGADO'
+    }
+  })
 }
 
 function WriteLedData(LED_id, status) {
